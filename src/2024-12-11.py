1252aae106a12f6cbb0cbf9db809b30052b85ea4
mga44 <39274754+mga44@users.noreply.github.com>
2024-12-28 23:44:10 +0100

    2024-12-11 (part 2)

--- src/2024-12-11.py
+++ src/2024-12-11.py
@@ -23,66 +23,30 @@ def compute(input:int) -> list:
     if input == 0:
         return [1]
     
-    num_size = len(str(num))
+    num_size = len(str(input))
     if num_size % 2 == 0:
         half = int(num_size/2)
         first_stone = int(str(input)[:half])
         second_stone = int(str(input)[half:])
         return [first_stone, second_stone]
     
-    return [num * 2024]
-
+    return [input * 2024]
 
 line = list(map(lambda x: int(x) ,utils.read_lines("resources\\2024-12-11.txt")[0].split(" ")))
 
-zero_cache = []
-easy_result_cache = {0:[1]}
+stone_freq = dict((x,1) for x in line)
 for i in range(75):
-    if i == 0:
-        zero_cache.append([0])
-        continue
-    
-    i_result = []
-    for num in zero_cache[i-1]:
-        if num in easy_result_cache:
-            r = easy_result_cache[num]
-            i_result.extend(r)
-        else:
-            r = compute(num)
-            i_result.extend(r)
-            easy_result_cache[num] = r
-        
-    zero_cache.append(i_result)
-    
-    
-
-
-zeroes = []
-result = line
-for i in range(75):
-    new_result = []
-    for num in result:
-        if isinstance(num, tuple):
-            zeroes.append(num)
-            continue
-        
-        if num in easy_result_cache:
-            new_result.extend(easy_result_cache[num])
-            continue
-        
-        num_size = len(str(num))
-        if num == 0:
-            new_result.append((0, i))
-        elif num_size % 2 == 0:
-            half = int(num_size/2)
-            first_stone = int(str(num)[:half])
-            second_stone = int(str(num)[half:])
-            new_result.append(first_stone)
-            new_result.append(second_stone)
-        else:
-            new_result.append(num * 2024)
-    result = new_result
+    tmp = {}
+    for stone, f in stone_freq.items():
+        new_stones = compute(stone)
+        for s in new_stones:
+            if s in tmp:
+                tmp[s] += f
+            else:
+                tmp[s] = f
+                
+    stone_freq = tmp
 
 
 
-print(f"Number of stones is {len(result)}")
+print(f"Number of stones is {sum(stone_freq.values())}")
